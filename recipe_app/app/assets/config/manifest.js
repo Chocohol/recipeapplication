5955fc497478d3d050ac54cb3eba773f5277f470
Chocohol <anelalani4@hotmail.com>
2016-07-14 15:52:43 -0700

    Version1
    version1

---
+++ recipe_app/app/assets/config/manifest.js
@@ -0,0 +1,10 @@
+// manifest.js
+//
+// JS bundles
+//= link ./javascripts/application.js
+//
+// CSS bundles
+//= link_tree ./stylesheets
+//
+// Pull in all app/assets/images/ since app/views may link to them
+//= link_tree ./images
