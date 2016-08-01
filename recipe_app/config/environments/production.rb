fba5af34981910879cd5604f3061f0770b579051
Chocohol <anelalani4@hotmail.com>
2016-07-31 22:53:34 -0700

    changes
    changes

--- recipe_app/config/environments/production.rb
+++ recipe_app/config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -77,4 +77,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Paperclip and ImageMagick
+  Paperclip.options[:command_path] = "C:\Program Files\ImageMagick-7.0.2-Q16\convert.exe"
 end
